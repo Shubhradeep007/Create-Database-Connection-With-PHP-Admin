0984b863267ca35a960230f314cc882d285d0e7b
Shubhradeep Bose <75801099+Shubhradeep007@users.noreply.github.com>
2021-05-16 12:59:35 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "school";
+$servername = "Server name";//For local host this is -> "Localhost";
+$username = "Your Usename";// For local host this is -> "root";
+$password = "your pass"; // For local host this nis null like -> "";
+$database = "Name of the Database Which you want to connect";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn){
